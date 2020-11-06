9e203dddd853d664edac29a527244ea9e7e789e7
jatkinosn <jeff.atkinosn@verizonmedia.com>
2020-11-06 15:28:22 -0500

    Removing previous build dir.

--- build/CMakeFiles/bro-plugin-Zeek_NOISE.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "__bro_plugin__"
-  "CMakeFiles/bro-plugin-Zeek_NOISE"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bro-plugin-Zeek_NOISE.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/lib/bif/__load__.zeek
+++
@@ -1,1 +0,0 @@
-@load ./events.bif.zeek
